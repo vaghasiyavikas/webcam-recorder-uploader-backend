b52361f1a1a92e0d674a5319eaacf02fdeec1204
vikas <vikas@smartworking-solutions.com>
2023-10-27 21:30:40 +0530

    Apply some comment changes

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,9 @@ const Minio = require('minio');
 const cors = require('cors');
 
 const app = express();
-app.use(cors());
+app.use(cors()); // Enable Cross-Origin Resource Sharing for the app
 
+// Initialize a Minio client
 const minioClient = new Minio.Client({
     endPoint: process.env.S3_ADDRESS,
     port: 443,
@@ -15,22 +16,28 @@ const minioClient = new Minio.Client({
     secretKey: process.env.S3_KEY_SECRET
 });
 
+// Configure multer to use memory storage
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
+// Route for handling the video upload
 app.post('/upload', upload.single('video'), (req, res) => {
+    // Check if a file has been uploaded
     if (!req.file) {
         return res.status(400).send('No video uploaded.');
     }
 
+    // Get the file buffer and create a unique file name
     const file = req.file.buffer;
     const fileName = `videos/${Date.now()}-${req.file.originalname}.webm`;
 
+    // Upload the file to the Minio server
     minioClient.putObject(process.env.S3_BUCKET, fileName, file, (err,) => {
         if (err) {
             return res.status(500).send('Error video uploading to Minio.');
         }
 
+        // Send a success message if the upload is successful
         return res.send('Video uploaded successfully.');
     });
 });
